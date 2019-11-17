41532f50a7f3ed4aff0920c8490054f8625ee47a
Clarisse B <klarisbadillo@gmail.com>
2019-11-17 11:10:24 +0800

    Toggle menu js for admission

--- js/main.js
+++ js/main.js
@@ -13,3 +13,15 @@ function navFunction() {
 } 
 
 //NAVIGATION BAR RESP
+
+
+
+//ACCORDION PROGRAM PAGE
+const items = document.querySelectorAll(".accordion a");
+
+function toggleAccordion() {
+  this.classList.toggle('active');
+  this.nextElementSibling.classList.toggle('active');
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
